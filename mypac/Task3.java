18ae2be53060adb8e5157c7b52b389d4e50bc3ee
EmoPesa21 <103261835+EmoPesa21@users.noreply.github.com>
2022-04-09 23:20:12 +0300

    Update Task3.java

--- mypac/Task3.java
+++ mypac/Task3.java
@@ -4,11 +4,11 @@ public class Task3 {
     public static void main(String[] args){
 
         System.out.print(Math.abs(-147));
-        System.out.println("\t");
+        System.out.print("\t");
         System.out.print(Math.abs(15));
-        System.out.println("\t");
+        System.out.print("\t");
         System.out.print(Math.abs(0));
-        System.out.println("\t");
+        System.out.print("\t");
         System.out.print(Math.abs(18));
     }
 }
