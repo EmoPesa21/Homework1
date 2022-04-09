93baf9644aeb8781f897f8a309740b1c402897eb
EmoPesa21 <103261835+EmoPesa21@users.noreply.github.com>
2022-04-09 23:21:46 +0300

    Update Task5.java

--- mypac/Task5.java
+++ mypac/Task5.java
@@ -2,11 +2,17 @@ package mypac;
 
 public class Task5 {
     public static void main(String[] args){
-        System.out.print(Math.pow(-20,2)); System.out.println("\t");
-        System.out.print(Math.pow(92,2)); System.out.println("\t");
-        System.out.print(Math.pow(638,2)); System.out.println("\t");
-        System.out.print(Math.pow(73,2)); System.out.println("\t");
-        System.out.print(Math.pow(200,2)); System.out.println("\t");
-        System.out.print(Math.pow(-22,2)); System.out.println("\t");
+        System.out.print(Math.pow(-20,2));
+        System.out.print("\t");
+        System.out.print(Math.pow(92,2));
+        System.out.print("\t");
+        System.out.print(Math.pow(638,2));
+        System.out.print("\t");
+        System.out.print(Math.pow(73,2));
+        System.out.print("\t");
+        System.out.print(Math.pow(200,2));
+        System.out.print("\t");
+        System.out.print(Math.pow(-22,2));
+        System.out.print("\t");
     }
 }
